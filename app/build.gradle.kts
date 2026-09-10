e5de744d9b71032443fbc7b44719eb17963c80d6
Pavel Janíček <pavel.janicek@live.com>
2026-09-10 21:09:53 +0200

    bump to 1.3.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.fpclient.android"
         minSdk = 26
         targetSdk = 36
-                versionCode = 30
-        versionName = "1.3.7"
+        versionCode = 31
+        versionName = "1.3.8"
     }
 
     signingConfigs {
