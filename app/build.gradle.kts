2125171808c7c033560926cd5c44aa8958c17cf3
Pavel Janíček <pavel.janicek@live.com>
2026-09-17 07:50:47 +0200

    2.0.0-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.fpclient.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 33
-        versionName = "1.4.0"
+        versionCode = 34
+        versionName = "2.0.0-alpha"
     }
 
     signingConfigs {
